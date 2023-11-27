6fbb3a993bc1e2937cdce5d7f12ac1d7734686d1
cfif1102 <96823032+cfif1102@users.noreply.github.com>
2023-11-27 19:05:33 +0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Kursovaja.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
